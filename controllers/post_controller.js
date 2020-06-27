36352d7eaba3f7900581312f86a443e579359a55
Abhimanyu Saraswat <swt.abhimanyu.31@gmail.com>
2020-06-27 22:22:37 +0530

    Deleted a post along with the comments related to that post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,18 +1,42 @@
-const Post=require('../models/posts');
-
-module.exports.post=function(req,res){
+const Post = require('../models/posts');
+const Comment = require('../models/comments');
+module.exports.post = function (req, res) {
     Post.create({
-        content:req.body.content,
-        user:req.user.id
-    },function(err,post){
-        if(err){
+        content: req.body.content,
+        user: req.user.id
+    }, function (err, post) {
+        if (err) {
             console.log('err in creating post');
             return;
         }
-            console.log('successfully posted');
-        
+        console.log('successfully posted');
+
     });
     res.redirect('back');
-}
+};
+
+
+module.exports.delete = function (req, res) {
+    Post.findById(req.params.id, function (err, post) {
+        if (err) { console.log('err if finding post during delete'); return; }
+        //checking if user logged in is the only that is deleting his post...logged in user==author of post to be deleted
+        if (post.user == req.user.id) {
+            post.remove();
+
+            Comment.deleteMany({ post: req.params.id }, function (err) {
+                if (err) {
+                    console.log('err in deleting comments');
+                    return;
+                }
+                
+            res.redirect('back');
+            });
+        }
+        else {
+            console.log('ERR!!!user deleting post doest not match with the user who posted the post');
+            res.redirect('back');
+        }
+    })
+};
 
 
--- routes/post.js
+++ routes/post.js
@@ -6,5 +6,5 @@ const postController=require('../controllers/post_controller');
 
 router.post('/create',passport.checkAuthentication,postController.post);
 
-
+router.get('/destory/:id',passport.checkAuthentication,postController.delete);
 module.exports=router;
