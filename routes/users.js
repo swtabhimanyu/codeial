6a2fa1fe0ae0a348dd61c6b42b6490046ecfeb77
Abhimanyu Saraswat <swt.abhimanyu.31@gmail.com>
2020-07-08 16:09:38 +0530

    Minor indentation changes

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,33 @@
-const express=require('express');
-const router=express.Router();
-const passport=require('passport');
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
 
-const userController=require('../controllers/userController');
+const userController = require('../controllers/userController');
 const { route } = require('.');
 
-router.post('/update/:id',passport.checkAuthentication,userController.update);
+router.post('/update/:id', passport.checkAuthentication, userController.update);
 
-router.get('/profile/:id',passport.checkAuthentication,userController.profile);
-router.get('/signUp',userController.signUp);  //render signUp page
-router.get('/signIn',userController.signIn);  //render signIn page
+router.get('/profile/:id', passport.checkAuthentication, userController.profile);
+router.get('/signUp', userController.signUp);  //render signUp page
+router.get('/signIn', userController.signIn);  //render signIn page
 
 
-router.post('/create',userController.create);    //EJS form for userSignUp posts req here for signUp of User
+router.post('/create', userController.create);    //EJS form for userSignUp posts req here for signUp of User
 
 
 //Ejs comes here and transfer control to passport-local for authentication
-router.post('/create-session' , passport.authenticate(      // EJS form for userSignIn posts req here for signIn of user
+router.post('/create-session', passport.authenticate(      // EJS form for userSignIn posts req here for signIn of user
     'local',  //strategy used in passport
-    {failureRedirect:'/user/signIn'}   //if user fails to signIn,i.e,passport returns err then signIn page is reloaded
-) ,userController.createSession  //if user sucessfully signIn then it transfer control to controller for create session
+    { failureRedirect: '/user/signIn' }   //if user fails to signIn,i.e,passport returns err then signIn page is reloaded
+), userController.createSession  //if user sucessfully signIn then it transfer control to controller for create session
 );
 
 
-router.get('/signOut',userController.destroySession);
+router.get('/signOut', userController.destroySession);
 
 
-router.get('/auth/google', passport.authenticate('google',{scope:['profile','email']}));
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/auth/google/callback', passport.authenticate('google',{failureRedirect:'/user/signIn'}),userController.createSession);
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/signIn' }), userController.createSession);
 
-module.exports=router;
+module.exports = router;
